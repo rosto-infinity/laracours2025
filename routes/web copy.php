962eb65ba7e55f374058f1384e1c512a8c832726
ROSTAND LELE <rostoinfocus@gmail.com>
2025-06-21 08:26:00 +0100

    install breeze from 'breeze';

--- routes/web copy.php
+++ routes/web copy.php
@@ -1,10 +1,7 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
-use Illuminate\Support\Facades\Route;
-
 use App\Models\Service;
-
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
 use App\Http\Controllers\Admin\AboutController;
 use App\Http\Controllers\Admin\MediaController;
@@ -82,20 +79,6 @@ Route::prefix('/admin/testimonials')->group(function () {
     Route::delete('/{id}', [TestimonialController::class, 'destroy'])->name('destroy-testimonial');
 });
 
-
-
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__.'/auth.php';
 
 Route::get('/{any}', function () {
     return view('notFoundPage');
