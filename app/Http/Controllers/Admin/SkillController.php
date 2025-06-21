221f44afbc315819a766a06b3182088a63b396b0
ROSTAND LELE <rostoinfocus@gmail.com>
2025-06-21 11:31:28 +0100

    debug bar

--- app/Http/Controllers/Admin/SkillController.php
+++ app/Http/Controllers/Admin/SkillController.php
@@ -3,13 +3,35 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\PageController;
+use App\Models\Experience;
+use App\Models\Media;
+use App\Models\Project;
+use App\Models\Service;
 use App\Models\Skill;
+use App\Models\Testimonial;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class SkillController extends Controller
 {
     public function index()
     {
+        $services= Service::orderBy('id', 'DESC')->get();
+        $services= Service::orderBy('id', 'DESC')->get();
+        $services= Service::orderBy('id', 'DESC')->get();
+        $projects= Project::orderBy('id', 'DESC')->get();
+        $medoas= Media::orderBy('id', 'DESC')->get();
+        $testimonials= Testimonial::orderBy('id', 'DESC')->get();
+        $experiences= Experience::orderBy('id', 'DESC')->get();
+        $users= User::orderBy('id', 'DESC')->get();
+        $projects= Project::orderBy('id', 'DESC')->get();
+        $medoas= Media::orderBy('id', 'DESC')->get();
+        $testimonials= Testimonial::orderBy('id', 'DESC')->get();
+        $experiences= Experience::orderBy('id', 'DESC')->get();
+        $users= User::orderBy('id', 'DESC')->get();
+     
+
         $skills= Skill::with('service')->orderBy('id', 'DESC')->get();
         return view('admin.skills.index-skills',compact('skills'));
     }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+         /*Model::automaticallyEagerLoadRelationships() est une fonctionnalité 
+       puissante introduite dans Laravel 12.8 qui simplifie la gestion des 
+       relations Eloquent en évitant les problèmes de requêtes N+1.
+        Elle permet d'écrire un code plus propre et plus maintenable,
+         tout en améliorant les performances de votre application.*/
+    // Model::automaticallyEagerLoadRelationships();
     }
 }
